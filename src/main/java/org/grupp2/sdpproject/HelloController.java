abdc70f3fed57dbaf3e0d3fa3b70175c9a329734
McSwede <danne-e@hotmail.com>
2025-03-27 10:24:02 +0100

    Add base project

---
+++ src/main/java/org/grupp2/sdpproject/HelloController.java
@@ -0,0 +1,14 @@
+package org.grupp2.sdpproject;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
