598d5fb2fbe1899f3f08621a31137b3076893954
McSwede <danne-e@hotmail.com>
2025-03-28 11:11:55 +0100

    Add City class

---
+++ src/main/java/org/grupp2/sdpproject/entities/City.java
@@ -0,0 +1,55 @@
+package org.grupp2.sdpproject.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "city")
+public class City {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "city_id")
+    private short cityId;
+
+    @Size(max = 50)
+    @Column(name = "city", nullable = false, length = 50)
+    private String city;
+
+    @ManyToOne
+    @JoinColumn(name = "country_id", nullable = false)
+    private Country country;
+
+    // Probably a OneToMany relation to Address
+
+    public City() {
+    }
+
+    public City(String city, Country country) {
+        this.city = city;
+        this.country = country;
+    }
+
+    public short getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(short cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+
+    public void setCountry(Country country) {
+        this.country = country;
+    }
+}
