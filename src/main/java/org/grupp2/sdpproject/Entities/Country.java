c0c014f3a5d5ee1fa4bc145104297e630992d7e5
McSwede <danne-e@hotmail.com>
2025-03-28 09:40:09 +0100

    Add Country class

---
+++ src/main/java/org/grupp2/sdpproject/Entities/Country.java
@@ -0,0 +1,43 @@
+package org.grupp2.sdpproject.Entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "country")
+public class Country {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "country_id")
+    private short countryId;
+
+    @Size(max = 50)
+    @Column(name = "country", nullable = false)
+    private String country;
+
+    // I believe this should have a OneToMany relationship to cities
+
+    public Country() {
+    }
+
+    public Country(short countryId, String country) {
+        this.countryId = countryId;
+        this.country = country;
+    }
+
+    public short getCountryId() {
+        return countryId;
+    }
+
+    public void setCountryId(short countryId) {
+        this.countryId = countryId;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
