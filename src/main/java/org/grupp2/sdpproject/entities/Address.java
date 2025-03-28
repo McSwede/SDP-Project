dc26b27a4f40af3277c0880af82087f1db27ae16
McSwede <danne-e@hotmail.com>
2025-03-28 13:22:36 +0100

    Add Address class

---
+++ src/main/java/org/grupp2/sdpproject/entities/Address.java
@@ -0,0 +1,105 @@
+package org.grupp2.sdpproject.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "address")
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "address_id")
+    private int addressId;
+
+    @Size(max = 50)
+    @Column(name = "address", nullable = false, length = 50)
+    private String address;
+
+    @Size(max = 50)
+    @Column(name = "address2", length = 50)
+    private String address2;
+
+    @Size(max = 20)
+    @Column(name = "district", nullable = false, length = 20)
+    private String district;
+
+    @ManyToOne
+    @JoinColumn(name = "city_id", nullable = false)
+    private City city;
+
+    @Size(max = 10)
+    @Column(name = "postal_code", length = 10)
+    private String postalCode;
+
+    @Size(max = 20)
+    @Column(name = "phone", nullable = false, length = 20)
+    private String phone;
+
+    public Address() {
+    }
+
+    public Address(String address, String address2, String district, City city, String postalCode, String phone) {
+        this.address = address;
+        this.address2 = address2;
+        this.district = district;
+        this.city = city;
+        this.postalCode = postalCode;
+        this.phone = phone;
+    }
+
+    public int getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(int addressId) {
+        this.addressId = addressId;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAddress2() {
+        return address2;
+    }
+
+    public void setAddress2(String address2) {
+        this.address2 = address2;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public City getCity() {
+        return city;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
