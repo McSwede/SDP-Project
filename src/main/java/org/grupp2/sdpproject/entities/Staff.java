b3717d768ae486fa8baabc43214a6f45412a9cda
JesperVaangman <Jesper.vaangman@hotmail.com>
2025-03-28 17:50:24 +0100

    added constructors

--- src/main/java/org/grupp2/sdpproject/entities/Staff.java
+++ src/main/java/org/grupp2/sdpproject/entities/Staff.java
@@ -1,4 +1,20 @@
 package org.grupp2.sdpproject.entities;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "staff")
 public class Staff {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "store_id")
+    private byte storeId;
+
+    @Column(name = "manager_staff_id", nullable = false, unique = true)
+    private byte managerStaffId;
+
+    @Column(name = "address_id", nullable = false)
+    private short addressId;
+
 }
