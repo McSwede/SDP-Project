5d7898baaf59794c7da3a5d41d83378ead142357
McSwede <danne-e@hotmail.com>
2025-04-02 15:19:49 +0200

    Made better example

--- src/main/java/org/grupp2/sdpproject/dao/FilmDAO.java
+++ src/main/java/org/grupp2/sdpproject/dao/FilmDAO.java
@@ -1,12 +1,24 @@
 package org.grupp2.sdpproject.dao;
 
 import org.grupp2.sdpproject.entities.Film;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
+import java.util.List;
+
 public class FilmDAO extends GenericDAO<Film> {
+    private final SessionFactory sessionFactory;
+
     public FilmDAO(SessionFactory sessionFactory) {
         super(Film.class, sessionFactory);
+        this.sessionFactory = sessionFactory;
     }
 
-    // This is where we would add overrides to the generic DAO methods
+    // Example of overridden method
+    @Override
+    public List<Film> findAll() {
+        try (Session session = sessionFactory.openSession()) {
+            return session.createQuery("FROM Film", Film.class).list();
+        }
+    }
 }
