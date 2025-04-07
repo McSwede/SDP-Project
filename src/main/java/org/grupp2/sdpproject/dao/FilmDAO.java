2ea41c24a9b78ed62366236e70695f39aa46740f
McSwede <danne-e@hotmail.com>
2025-04-07 08:55:27 +0200

    Remove example code

--- src/main/java/org/grupp2/sdpproject/dao/FilmDAO.java
+++ src/main/java/org/grupp2/sdpproject/dao/FilmDAO.java
@@ -1,27 +1,15 @@
 package org.grupp2.sdpproject.dao;
 
-import org.grupp2.sdpproject.Utils.DAOManager;
 import org.grupp2.sdpproject.entities.Film;
-import org.grupp2.sdpproject.entities.FilmText;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.hibernate.query.Query;
 
-import java.util.List;
 
 public class FilmDAO extends GenericDAO<Film> {
 
     public FilmDAO(SessionFactory sessionFactory) {
         super(Film.class, sessionFactory);
-    }
-
-    // Example of overridden method
-    @Override
-    public List<Film> findAll() {
-        try (Session session = sessionFactory.openSession()) {
-            return session.createQuery("FROM Film", Film.class).list();
-        }
     }
 
     @Override
