2bfb9647d3e0212cf78b5714f88ae377b72334c2
McSwede <danne-e@hotmail.com>
2025-04-08 14:51:06 +0200

    Save volume to config and add slider to CustomerDashBoardScene

--- src/main/java/org/grupp2/sdpproject/GUI/customer/CustomerDashBoardScene.java
+++ src/main/java/org/grupp2/sdpproject/GUI/customer/CustomerDashBoardScene.java
@@ -3,9 +3,15 @@ package org.grupp2.sdpproject.GUI.customer;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import org.grupp2.sdpproject.GUI.SceneController;
+import org.grupp2.sdpproject.Utils.ConfigManager;
 import org.grupp2.sdpproject.Utils.SessionManager;
+import org.grupp2.sdpproject.Utils.SoundManager;
+
+import java.io.IOException;
 
 public class CustomerDashBoardScene {
 
@@ -18,13 +24,33 @@ public class CustomerDashBoardScene {
     @FXML private Button accountDetailsButton;
     @FXML private Button logoutButton;
     @FXML private Button colorsheme;
+    @FXML private Button soundButton;
+    @FXML private Slider volumeSlider;
 
     private final SceneController sceneController = SceneController.getInstance();
+    private final SoundManager soundManager = SoundManager.getInstance();
 
     @FXML
     public void initialize() {
         boolean isDarkMode = sceneController.isDarkMode();
         colorsheme.setText(isDarkMode ? "Ljust läge" : "Mörkt läge");
+
+        ConfigManager configManager = sceneController.getConfigManager();
+        double savedVolume = Double.parseDouble(configManager.getProperty("music.volume", "0.1"));
+        soundManager.setGlobalVolume(savedVolume);
+        volumeSlider.setValue(savedVolume);
+
+        volumeSlider.valueProperty().addListener((observable, oldValue, newValue) -> {
+            double volume = newValue.doubleValue();
+            soundManager.setGlobalVolume(volume);
+
+            try {
+                configManager.setProperty("music.volume", String.valueOf(volume));
+                configManager.saveConfig();
+            } catch (IOException e) {
+                System.err.println("Failed to save volume setting: " + e.getMessage());
+            }
+        });
     }
 
     @FXML
@@ -71,5 +97,26 @@ public class CustomerDashBoardScene {
         root.getStylesheets().add(styleSheet);
     }
 
+    @FXML private void toggleMusic() {
+        if (soundManager.isPaused()) {
+            soundManager.resumeCurrentMusic();
+            soundButton.setText("Pausa musik");
+        }
+        else {
+            soundManager.pauseCurrentMusic();
+            soundButton.setText("Spela musik");
+        }
+    }
 
+    @FXML private void handleVolumeChange(MouseEvent event) {
+        double volume = volumeSlider.getValue();
+        soundManager.setGlobalVolume(volume);
+
+        try {
+            sceneController.getConfigManager().setProperty("music.volume", String.valueOf(volume));
+            sceneController.getConfigManager().saveConfig();
+        } catch (IOException e) {
+            System.err.println("Failed to save volume setting: " + e.getMessage());
+        }
+    }
 }
